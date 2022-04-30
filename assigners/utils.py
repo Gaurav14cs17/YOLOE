d899b901f9d25d47b9a68ba52327fab42ac7cb89
Gaurav14cs17 <gouravkumar717@gmail.com>
2022-04-30 13:18:21 +0530

    loss

--- assigners/utils.py
+++ assigners/utils.py
@@ -3,15 +3,11 @@ import torch.nn.functional as F
 import torch.nn as nn
 
 
-def generate_anchors_for_grid_cell(feats,
-                                   fpn_strides,
-                                   grid_cell_size=5.0,
-                                   grid_cell_offset=0.5):
+def generate_anchors_for_grid_cell(feats,fpn_strides,grid_cell_size=5.0,grid_cell_offset=0.5):
     anchors = []
     anchors_points = []
     num_anchors_list = []
     stride_tensor = []
-
     for feat, stride in zip(feats, fpn_strides):
         _, _, h, w = feat.shape
         cell_half_size = (grid_cell_size * stride) * 0.5
@@ -28,7 +24,6 @@ def generate_anchors_for_grid_cell(feats,
         anchors.append(anchor.reshape([-1, 4]))
         anchors_points.append(anchor_point.reshape([-1, 2]))
         num_anchors_list.append(len(anchors[-1]))
-
         stride_tensor.append(stride_tensor.append(torch.full([num_anchors_list[-1], 1], stride, dtype=feat.dtype)))
 
     anchors = torch.concat(anchors)
