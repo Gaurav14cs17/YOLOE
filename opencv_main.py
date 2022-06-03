e20969f87ea7992c49fafc91557a730d20038b8a
Gaurav <33745822+Gaurav14cs17@users.noreply.github.com>
2022-06-03 21:14:54 +0530

    Update opencv_main.py

--- opencv_main.py
+++ opencv_main.py
@@ -1,17 +1,1 @@
-# pip install opencv-python
-# conda install opencv-puython
 
-
-import cv2
-
-image_path = "C:/Users/goura/OneDrive/Pictures/Photo-1.jpeg"
-
-image = cv2.imread(image_path )
-# (0 , 255)
-
-
-cv2.rectangle(image, (200, 300), (500, 500), (0, 255, 0), 10)
-cv2.circle(image, (200, 300), 50, (0, 0, 255), 10)
-
-cv2.imshow("Image", image)
-cv2.waitKey(0)
